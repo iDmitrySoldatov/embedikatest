3be62a4f8ce54adf8d4a65c07c3ee26d726b2d0b
Dmitry Soldatov <dmitriysoldatov1998@gmail.com>
2023-01-18 11:19:47 +0500

    Create project

---
+++ src/main/java/ru/embedika/test/embedikatest/EmbedikatestApplication.java
@@ -0,0 +1,13 @@
+package ru.embedika.test.embedikatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedikatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbedikatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/embedika/test/embedikatest/EmbedikatestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.embedika.test.embedikatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedikatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
